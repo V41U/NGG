30e18efea24f776e2ae60a64e044148596fe338d
Valentin Sagl <valentin.sagl@gmail.com>
2020-11-10 16:56:21 +0100

    Initial commit

---
+++ Source/NGG/NGGAlgorithmBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "NGGAlgorithmBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NGG_API UNGGAlgorithmBase : public UObject
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/NGG/NGGGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NGGGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NGG_API ANGGGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
